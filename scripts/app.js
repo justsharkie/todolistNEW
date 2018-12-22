6b49200a00bfc4b28e4f1f6c8f73c0251164cd15
Shayna <shaynawark@gmail.com>
2018-12-22 10:47:20 -0500

    no injector error anymore
    forgot to load AngularFire.... which is quite important in this case.

--- scripts/app.js
+++ scripts/app.js
@@ -1,29 +1,31 @@
-angular 
-.module('myApp', ['ngRoute', 'firebase'])
-// Initialize Firebase
-.constant('firebaseConfig', {
-    apiKey: "AIzaSyCxp97FqBuCDRNKs2F7IcviGRot-USebW4",
-    authDomain: "to-do-list-revamp.firebaseapp.com",
-    databaseURL: "https://to-do-list-revamp.firebaseio.com",
-    projectId: "to-do-list-revamp",
-    storageBucket: "to-do-list-revamp.appspot.com",
-    messagingSenderId: "1024744737533"
-}) // end constant
-.run(function (firebaseConfig) {
-    firebase.initializeApp(firebaseConfig);
-}) // end run
-.controller('todoCtrl', function($scope, $firebaseObject, $firebaseArray) {
-    var dbref = firebase.database().ref().child('todos')
-    this.blankTodo = function () {
-        ({
-            title: ''
-        })
-    }
-    $scope.todos = $firebaseArray(dbref)
-    $scope.newTodo = this.blankTodo()
-    $scope.addTodo = function () {
-        $scope.newTodo.completed = false;
-        $scope.todos.$add($scope.newTodo)
+    angular
+    .module('myApp', ['ngRoute', 'firebase'])
+    .constant('firebaseConfig', {
+        apiKey: "AIzaSyCxp97FqBuCDRNKs2F7IcviGRot-USebW4",
+        authDomain: "to-do-list-revamp.firebaseapp.com",
+        databaseURL: "https://to-do-list-revamp.firebaseio.com",
+        projectId: "to-do-list-revamp",
+        storageBucket: "to-do-list-revamp.appspot.com",
+        messagingSenderId: "1024744737533"
+    })
+    .run(function (firebaseConfig){
+        firebase.initializeApp(firebaseConfig)
+    })
+    .controller('todoCtrl', function ($scope, $firebaseObjecy, $firebaseArray) {
+        // Should be setting up the database...
+        var dbRef = firebase.database().ref().child('todos')
+        $scope.todos = $firebaseArray(dbRef)
+        
+        this.blankTodo = function () {
+            ({
+                title: ''
+            })
+        }
+        
         $scope.newTodo = this.blankTodo()
-    }
-}) 
+        // Adding a new To Do Item
+        $scope.addTodo = function () {
+            $scope.todos.$add($scope.newTodo)
+            $scope.newTodo = this.blankTodo()
+        }
+    })
